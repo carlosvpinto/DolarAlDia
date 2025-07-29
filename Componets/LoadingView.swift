eabd352d8bfc27b6e4ab7010e494fdccdbb32e21
carlosvpinto <desoftis@gmail.com>
2025-07-29 13:57:00 -0400

    Casi terminada con vibracion y animaciones mejoradas

---
+++ Componets/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  DolarAlDia
+//
+//  Created by Carlos Vicente Pinto on 7/29/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
