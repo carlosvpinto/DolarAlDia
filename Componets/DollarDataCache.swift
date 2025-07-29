70679472bd6c9ae68ca0a0c8a42c7d69187c18a3
carlosvpinto <desoftis@gmail.com>
2025-07-28 21:54:40 -0400

    Con Toolbar Abajo Bien

---
+++ Componets/DollarDataCache.swift
@@ -0,0 +1,18 @@
+//
+//  DollarDataCache.swift
+//  DolarAlDia
+//
+//  Created by Carlos Vicente Pinto on 7/28/25.
+//
+
+import SwiftUI
+
+struct DollarDataCache: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DollarDataCache()
+}
