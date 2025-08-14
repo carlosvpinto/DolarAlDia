bd62fb5c060c67f9b75953ee1433f581e714c672
carlosvpinto <desoftis@gmail.com>
2025-08-13 21:19:55 -0400

    version con vista de Binance

---
+++ DolarAlDia/Preview Content/PlatformRatesView.swift
@@ -0,0 +1,18 @@
+//
+//  PlatformRatesView.swift
+//  DolarAlDia
+//
+//  Created by Carlos Vicente Pinto on 8/13/25.
+//
+
+import SwiftUI
+
+struct PlatformRatesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlatformRatesView()
+}
